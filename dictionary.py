e011dd837d0f943e48f2f5d361e06d79acdffc28
Karan479-blip <kc3767643@gmail.com>
2024-06-15 00:57:13 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+dict1={"karan":"gym","nishant":"mac","aaditya":"water"}
+print(dict1)
+
+for x in dict1: 
+    print(x)  
+for x in dict1.values():
+    print(x)
+    
+for x in dict1.items():
+    print(x)
+
+
+dict1['key4']='value4'
+print(dict1)
+
